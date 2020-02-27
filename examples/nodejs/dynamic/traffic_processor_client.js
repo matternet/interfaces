3339198f8e2a4591db8de85d1058a2aabc9584ac
Adolfo Martinelli <659782+adolfo@users.noreply.github.com>
2020-02-27 12:32:23 -0800

    [examples] Add auth metadata to node.js example (#24)

--- examples/nodejs/dynamic/traffic_processor_client.js
+++ examples/nodejs/dynamic/traffic_processor_client.js
@@ -1,25 +1,28 @@
 // Bootstrap our dependencies.
-var grpc = require('grpc');
-var pl = require('@grpc/proto-loader');
+const grpc = require('grpc');
+const pl = require('@grpc/proto-loader');
+const request = require('request-promise')
 
+const AUTH_ENDPOINT = 'auth.airmap.com'
+const API_ENDPOINT = 'api.airmap.com:443'
 
-// Endpoint points to sandbox environment
-const ENDPOINT = 'stage.api.airmap.com:443'
-
+// Configure with your client id and client secret
+const CLIENT_ID = '{CLIENT_ID}'
+const CLIENT_SECRET = '{CLIENT_SECRET}'
 
 // Load the gRPC package definitions for the traffic service.
-var packageDefinition = pl.loadSync(
+const packageDefinition = pl.loadSync(
   [
-      'collector.proto',
-      'identity.proto',
-      'track.proto',
-      'emitter.proto',
-      'sensors.proto',
-      'status.proto',
-      'ack.proto',
-      'measurements.proto',
-      'units.proto',
-      'ids.proto'
+    'collector.proto',
+    'identity.proto',
+    'track.proto',
+    'emitter.proto',
+    'sensors.proto',
+    'status.proto',
+    'ack.proto',
+    'measurements.proto',
+    'units.proto',
+    'ids.proto'
   ],
   {
     keepCase: true,
@@ -29,40 +32,73 @@ var packageDefinition = pl.loadSync(
     oneofs: true,
 
     includeDirs: [
-     __dirname + '/../../../grpc/tracking',
-     __dirname + '/../../../grpc/system',
-     __dirname + '/../../../grpc/measurements',
-     __dirname + '/../../../grpc/units',
-     __dirname + '/../../../grpc/ids',
-     __dirname + "/node_modules/google-proto-files"
+      __dirname + '/../../../grpc/tracking',
+      __dirname + '/../../../grpc/system',
+      __dirname + '/../../../grpc/measurements',
+      __dirname + '/../../../grpc/units',
+      __dirname + '/../../../grpc/ids',
+      __dirname + "/node_modules/google-proto-files"
     ]
-});
-var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-var tracking = protoDescriptor.tracking
+  });
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+const tracking = protoDescriptor.tracking
 
-// Create a client for connecting to the collector.
-var collector = new tracking.Collector(ENDPOINT, grpc.credentials.createSsl());
-var client = collector.ConnectProcessor();
+async function authenticate() {
 
-client.on('data', function (response) {
-  switch (response.details) {
-    case "batch":
-      for (let i in response.batch.tracks) {
-        let track = response.batch.tracks[i]
-        console.log(track.identities)
-        console.log(track.position.absolute)
-      }
-      break
-    case "status":
-      console.log('received status from collector: ', response.status.level, response.status.message)
-      break
+  const options = {
+    uri: `https://${AUTH_ENDPOINT}/realms/airmap/protocol/openid-connect/token`,
+    method: 'POST',
+    form: {
+      grant_type: "client_credentials",
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET
+    }
   }
-})
 
-client.on('end', function() {
-  console.log('client connection to collector ended')
-})
+  try {
+    const response = await request(options);
+    const body = JSON.parse(response)
+    return body.access_token
+  } catch (error) {
+    throw error
+  }
+}
 
-client.on('error', function(e) {
-  console.log('client connection to collector entered error state: ', e)
-})
+function connectProcessor(token) {
+
+  const collector = new tracking.Collector(API_ENDPOINT, grpc.credentials.createSsl());
+
+  const metadata = new grpc.Metadata();
+  metadata.add("authorization", "Bearer " + token);
+
+  const client = collector.ConnectProcessor(metadata);
+
+  client.on('data', function (response) {
+    switch (response.details) {
+      case "batch":
+        for (let i in response.batch.tracks) {
+          let track = response.batch.tracks[i]
+          console.log(track.identities)
+          console.log(track.position.absolute)
+        }
+        break
+      case "status":
+        console.log('received status from collector: ', response.status.level, response.status.message)
+        break
+    }
+  })
+
+  client.on('end', function () {
+    console.log('client connection to collector ended')
+  })
+
+  client.on('error', function (e) {
+    console.log('client connection to collector entered error state: ', e)
+  })
+
+}
+
+// Login using the client credentials and connect to the collector with a token
+authenticate()
+  .then(connectProcessor)
+  .catch(console.log)
